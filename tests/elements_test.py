8b190e34800b5bea610e7bece4f14fb2cd32c680
Lasloflo <qrash1992@gmail.com>
2023-01-14 23:06:40 +0300

    add faker data generate

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -6,10 +6,9 @@ from pages.elements_page import TextBoxPage
 def test_text_box(driver):
     text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
     text_box_page.open()
-    text_box_page.fill_all_fields()
+    full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
     output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
-    print(output_name)
-    print(output_email)
-    print(output_cur_addr)
-    print(output_per_addr)
-    time.sleep(5)
+    assert full_name == output_name, "the full_name does not mathc"
+    assert email == output_email, "the email does not mathc"
+    assert current_address == output_cur_addr, "the current_url does not mathc"
+    assert permanent_address == output_per_addr, "the permanent does not mathc"
