fb6c3c1bcf3d3e2e01ef7ba8549144d8187faae4
Lasloflo <qrash1992@gmail.com>
2023-01-16 10:16:33 +0300

    add checking for checkbox elements

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,14 +1,30 @@
 import time
 
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
-def test_text_box(driver):
-    text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
-    text_box_page.open()
-    full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
-    output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
-    assert full_name == output_name, "the full_name does not mathc"
-    assert email == output_email, "the email does not mathc"
-    assert current_address == output_cur_addr, "the current_url does not mathc"
-    assert permanent_address == output_per_addr, "the permanent does not mathc"
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            text_box_page.open()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
+            assert full_name == output_name, "the full_name does not mathc"
+            assert email == output_email, "the email does not mathc"
+            assert current_address == output_cur_addr, "the current_url does not mathc"
+            assert permanent_address == output_per_addr, "the permanent does not mathc"
+
+    class TestCheckbox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, "checkboxes have not been selected"
+            time.sleep(10)
+
+# pytest -s -v tests/elements_test.py::TestElements::TestCheckbox::test_check_box
